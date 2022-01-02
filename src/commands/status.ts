b42b772bd1a516dd8f0eb8c945be2df30dfd52df
Kyle <KyleMit@gmail.com>
2022-01-02 15:55:42 -0500

    refactor print group

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,4 +1,5 @@
 import { Command, Option } from "commander"
+import { stringify } from "querystring";
 import { GitStatusGroups, IStatusOptions, StatusFilterTypes, StatusOrderTypes } from "../models";
 import { IGitStatus } from "../models/models";
 import { boolCompare, formatNumber, getCurrentWorkingDirectory, getGitDirectories, getGitStatusInfo, mapAsync, printBlue, printBold, printCyan, printDim, printGreen, printRed, printUnderscore, printYellow } from "../utils"
@@ -10,7 +11,7 @@ export const statusCmd = new Command('status')
     .option('-s, --short', 'show statuses in a single line per repo')
     .option('-h, --hide-headers', 'hide group headers in output')
     .addOption(new Option('-f, --filter <filter>', 'filter results').choices(Object.values(StatusFilterTypes)).default(StatusFilterTypes.all))
-    .addOption(new Option('-o, --order <sort>', 'sort order').choices(Object.values(StatusOrderTypes)).default(StatusOrderTypes.status))
+    // .addOption(new Option('-o, --order <sort>', 'sort order').choices(Object.values(StatusOrderTypes)).default(StatusOrderTypes.status))
     .action(statusAction)
 
 
@@ -34,47 +35,48 @@ async function statusAction(opts: IStatusOptions) {
             return acc;
     }, new GitStatusGroups())
 
-    const modifiedRepo = (r: IGitStatus) => `${printBold(printBlue(r.name))} ${printDim(printBlue(`(${r.branch})`))}`
-    const modificationsCount = (r: IGitStatus) => ` ${formatNumber(r.modifiedCount.insertions)}(+) ${formatNumber(r.modifiedCount.deletions)}(-)`
+    const modifiedRepo = (repo: IGitStatus) => `${printBold(printBlue(repo.name))} ${printDim(printBlue(`(${repo.branch})`))}`
+    const modificationsCount = (repo: IGitStatus) => ` ${formatNumber(repo.modifiedCount.insertions)}(+) ${formatNumber(repo.modifiedCount.deletions)}(-)`
     const printHeader = (text: string) => {
         if (opts.hideHeaders) return;
         console.log('\n' + printYellow(text))
     }
 
-
-
-    printHeader('Unsaved Changes')
-    grouped.tooManyChanges.forEach(repo => {
+    const printTooManyChanges = (repo: IGitStatus) => {
         const message = !opts.short
             ? `\n${printRed(`Too many changes ${printDim('- run manually with:')}`)}\n${printUnderscore(`$ git -C '${repo.path}' status`)}\n`
             : ` ${modificationsCount(repo)}`
         console.log(`${modifiedRepo(repo)}${message}`)
-    })
+    }
 
-    grouped.isDirty.forEach(repo => {
+    const printChanges = (repo: IGitStatus) => {
         const message = !opts.short
             ? `\n${repo.status}`
             : ` ${modificationsCount(repo)}`
         console.log(`${modifiedRepo(repo)}${message}`)
-    })
-
-    const logSync = (r: IGitStatus) => {
-        const message = !opts.short
-            ? `\n${r.diffCommitCount.ahead} commits(s) ahead, ${r.diffCommitCount.behind} commits(s) behind`
-            : ` ${r.diffCommitCount.ahead}↑ ${r.diffCommitCount.behind}↓`
-        console.log(`${modifiedRepo(r)}${message}`)
     }
 
-    printHeader('Unpushed Commits')
-    grouped.hasUnmergedCommits.forEach(logSync)
+    const printUnsyncedCommits = (repo: IGitStatus) => {
+        const message = !opts.short
+            ? `\n${repo.diffCommitCount.ahead} commits(s) ahead, ${repo.diffCommitCount.behind} commits(s) behind`
+            : ` ${repo.diffCommitCount.ahead}↑ ${repo.diffCommitCount.behind}↓`
+        console.log(`${modifiedRepo(repo)}${message}`)
+    }
 
-    printHeader('Behind Origin')
-    grouped.hasUnsyncedCommits.forEach(logSync)
-
-    printHeader('Up To Date')
-    grouped.upToDate.forEach(repo => {
+    const printUpToDate = (repo: IGitStatus) => {
         console.log(`${printGreen(repo.name)} (${printDim(printGreen(repo.branch))}) ${printDim('Up to date')}`)
-    })
+    }
+
+    const printGroup = (header: string, repos: Array<IGitStatus>, printFn: (repo: IGitStatus) => void) => {
+        if (repos.length) { printHeader(header) }
+        repos.forEach(printFn)
+    }
+
+    printGroup('Too Many Changes', grouped.tooManyChanges, printTooManyChanges)
+    printGroup('Unsaved Changes', grouped.isDirty, printChanges)
+    printGroup('Unpushed Commits', grouped.hasUnmergedCommits, printUnsyncedCommits)
+    printGroup('Behind Origin', grouped.hasUnsyncedCommits, printUnsyncedCommits)
+    printGroup('Up To Date', grouped.upToDate, printUpToDate)
 
     console.log()
 }
